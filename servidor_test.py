7cc40237c5465561977f02d214c7ebc5be9c6b9f
AlexManoel-dev <82236308+AlexManoel-dev@users.noreply.github.com>
2021-05-07 16:38:11 -0300

    Update servidor_test.py

--- servidor_test.py
+++ servidor_test.py
@@ -1,9 +1,20 @@
-import servidor
+import unittest
 
-def test_index():
-    servidor.app.testing = True
-    client = servidor.app.test_client()
+class TestStringMethods(unittest.TestCase):
 
-    r = client.get('/')
-    assert r.status_code == 200
-    assert 'Hello World!' in r.data.decode('utf-8')
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
